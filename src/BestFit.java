e5a6f25bf4b0771697dbe800cd1203cc1612d743
matheo <matheomarumo@gmail.com>
2021-07-16 18:53:59 -0300

    - Comentários

--- src/BestFit.java
+++ src/BestFit.java
@@ -9,12 +9,17 @@ public class BestFit {
 
     }
 
-    /*
-    Retorna um ArrayList com os index dos quadros a serem utilzados para alocar uma certa quantidade de bytes
+    /**
+     * Obtem um ArrayList com os indices dos quadros a serem utilzados para alocar uma certa quantidade de bytes com base
+     * no algoritmo Best-fit
+     * @param bytes a quantidade de bytes a ser alocada
+     * @param mapaBits o mapa de bits que corresponde aos quadros
+     * @return uma lista com os indices dos quadros a serem alocados
      */
     public ArrayList<Integer> allocate(int bytes, boolean[] mapaBits) {
         ArrayList<int[]> espacosContiguos = new ArrayList<>();
 
+        //Procura por espaços contíguos e armazena o inicio e fim de cada um
         for (int i = 0; i < mapaBits.length; i++) {
             int fimEspaco = -1;
             int inicioEspaco = i;
@@ -27,26 +32,28 @@ public class BestFit {
             }
         }
 
+        //Busca o menor buraco para alocar aquela quantidade de bytes
         int[] menorBuraco = buscaBuraco(true, espacosContiguos, bytes);
-        int indexMenorBuraco = menorBuraco[0];
-        int tamanhoMenorBuraco = menorBuraco[1];
+        int indexMenorBuraco = menorBuraco[0];  //indice do menor buraco
+        int tamanhoMenorBuraco = menorBuraco[1];    //tamanho do menor buraco
         ArrayList<Integer> indexBuracosNaoContiguos = new ArrayList<>();
         if (tamanhoMenorBuraco == 0) {      //Caso não encontre um buraco contíguo grande o suficiente
             ArrayList<int[]> buracosNaoContiguos = new ArrayList<>();
             do{
-                if (tamanhoMenorBuraco == 0) {
+                if (tamanhoMenorBuraco == 0) {      //Caso não tenha encontrado um menor buraco para alocar a quantidade de bytes
+                    //Busca o maior buraco contíguo que não irá alocar todos os bytes
                     int[] maiorBuraco = buscaBuraco(false, espacosContiguos, bytes);
                     int indexMaiorBuraco = maiorBuraco[0];
                     int tamanhoMaiorBuraco = maiorBuraco[1];
                     buracosNaoContiguos.add(maiorBuraco);  //Adiciona index
-                    bytes -= tamanhoMaiorBuraco;
+                    bytes -= tamanhoMaiorBuraco;    //Subtrai do total de bytes
 
                     //Caso não encontre mais buracos retorna nulo
                     if (indexMaiorBuraco == -1 && indexMenorBuraco == -1) {
                         return null;
                     }
                 }
-
+                //Busca novamente o menor buraco possível para alocar a nova quantidade de bytes
                 menorBuraco = buscaBuraco(true, espacosContiguos, bytes);
                 indexMenorBuraco = menorBuraco[0];
                 tamanhoMenorBuraco = menorBuraco[1];
@@ -54,36 +61,42 @@ public class BestFit {
                 if (bytes <= 0) {
                     buracosNaoContiguos.add(menorBuraco);
                 }
-            } while (bytes > 0);
+            } while (bytes > 0);    //Enquanto não tiver encontrado espaços suficientes para alocar todos os bytes
+
 
             for (int[] item: buracosNaoContiguos) {
-                int tamanho = item[1]/32;
+                int tamanho = item[1]/32;       //Divide o numero do quadro por 32 para encontrar o indice
                 for (int i = item[0]; i < tamanho; i++){
-                    indexBuracosNaoContiguos.add(i);
+                    indexBuracosNaoContiguos.add(i);    //Adiciona indice do quadro
                 }
             }
-            Collections.sort(indexBuracosNaoContiguos);
+            Collections.sort(indexBuracosNaoContiguos);     //Ordena
             return indexBuracosNaoContiguos;
+
         } else {    //Caso encontre um boraco contíguo grande o suficiente
-            int quadrosNecessarios = (int) Math.ceil(bytes/32.0);
+            int quadrosNecessarios = (int) Math.ceil(bytes/32.0);   //Quantidade de quadros necessários para alocar os bytes
             ArrayList<Integer> buracosContiguos = new ArrayList<>();
 
             for (int i = indexMenorBuraco; i < indexMenorBuraco + quadrosNecessarios; i++) {
-                buracosContiguos.add(i);
+                buracosContiguos.add(i);    //Adiciona cada índice de cada quadro
             }
             return buracosContiguos;
         }
     }
 
-    /*
-     * Busca o menor ou maior buraco
+    /**
+     * Obtém o menor ou maior buraco
      * @param menor espeficica se o método vai buscar o menor (true) ou o maior buraco (false)
+     * @param espacosContiguos a lista com os espaços contíguos
+     * @param bytes o tamanho do dado a ser alocado
+     * @return array de inteiros onde a primeira posição contém o indice do quadro e a segunda contém o tamanhp
      */
     private int[] buscaBuraco(boolean menor, ArrayList<int[]> espacosContiguos, int bytes) {
         if (menor) {
             int menorBuraco = 0;
             int tamanho;
             int indexMenorBuraco = -1;
+            //Para cada espaço contiguo confere o seu tamanho e busca o menor buraco que aloque a quantidade de bytes
             for (int[] i : espacosContiguos) {
                 tamanho = (i[1] - i[0]) * 32;
                 if (tamanho >= bytes) {
@@ -99,7 +112,7 @@ public class BestFit {
                 }
             }
             return new int[]{indexMenorBuraco, menorBuraco};
-        } else {
+        } else {    //Busca o maior buraco que não aloque a quantidade de bytes
             int maiorBuraco = 0;
             int tamanho;
             int indexMaiorBuraco = -1;
